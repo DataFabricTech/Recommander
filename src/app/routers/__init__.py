91677b438db7b7fa72c9a568f0313001afa53f48
koseungbeom <vkdkf55@naver.com>
2024-09-04 11:20:40 +0900

    [Recommender] - embedding_router 추가 - clustering_router 용어 변경   - inference -> clustering

--- src/app/routers/__init__.py
+++ src/app/routers/__init__.py
@@ -1,9 +1,11 @@
 from .training_service import router as training_router
-from .clustering_service import router as inference_router
+from .clustering_service import router as clustering_router
+from .embedding_service import router as embedding_router
 from .check import router as check_router
 
 routers = [
     check_router,
     training_router,
-    inference_router
+    clustering_router,
+    embedding_router
 ]
